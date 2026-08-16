2b8c06d194391fe866db2488dad5035baa4326ac
Muhammed Abdelhadi <mano299ahmed1@gmail.com>
2026-08-16 06:28:19 +0300

    update url_launcher func

--- lib/data/url_launcher.dart
+++ lib/data/url_launcher.dart
@@ -4,10 +4,9 @@ import 'package:url_launcher/url_launcher.dart';
 Future<void> launchUrlLink(String url) async {
   final uri = Uri.parse(url);
 
-  if (await canLaunchUrl(uri)) {
-    await launchUrl(
-      uri,
-      mode: LaunchMode.externalApplication,
-    );
+  try {
+    await launchUrl(uri);
+  } catch (e) {
+    print('Error: $e');
   }
 }
